20996779507b1a3f1949fc59fdcf17698a7dc1d3
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-26 22:48:59 -0300

    adds equipo model with base attributes

---
+++ computerNetworkManager/src/cnm/model/Equipo.java
@@ -0,0 +1,15 @@
+package cnm.model;
+
+import java.util.List;
+
+public class Equipo {
+    // atributos
+    private Integer id;
+    private String codigo;
+    private String descripcion;
+    private String marca;
+    private String modelo;
+    private List<String> direccionesIp;
+    
+    
+}
