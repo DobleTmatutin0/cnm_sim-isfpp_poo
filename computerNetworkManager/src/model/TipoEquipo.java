6b9630e5687c7ceb99e1117af80ed836b7c95e49
DobleTmatutin0 <zz_00024@hotmail.com>
2026-07-10 21:14:30 -0300

    modify hashcode and equals now compares using id

--- computerNetworkManager/src/model/TipoEquipo.java
+++ computerNetworkManager/src/model/TipoEquipo.java
@@ -5,7 +5,7 @@ public class TipoEquipo {
     private long id;
     private String codigo;
     private String descripcion;
-   
+
     // Getters
     public long getId() {
         return id;
@@ -26,36 +26,28 @@ public class TipoEquipo {
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
-    
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
 
-
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        TipoEquipo other = (TipoEquipo) obj;
-        if (codigo == null) {
-            if (other.codigo != null)
-                return false;
-        } else if (!codigo.equals(other.codigo))
-            return false;
-        return true;
+        return Long.hashCode(this.id);
     }
 
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        TipoEquipo other = (TipoEquipo) obj;
+        return this.id == other.id;
+    }
+
+
 }
